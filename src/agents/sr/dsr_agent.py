dc359d2eea470ce496aa4e6b7f67ff851ae0d431
rychly <rychly@fortiss.org>
2019-11-29 16:51:19 +0100

    Deep Successor Representation Agent

--- src/agents/sr/dsr_agent.py
+++ src/agents/sr/dsr_agent.py
@@ -1,35 +1,135 @@
 import numpy as np
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 
 from src.agents.base_agent import BaseAgent
-from src.agents.dqn.qnet import QNet
+from src.agents.sr.dsr_net import DSRNet
 from src.agents.dqn.buffer_simple import SimpleBuffer
 
 
-class SRAgent(BaseAgent):
+class DSRAgent(BaseAgent):
   """Agent based on Successor Representations."""
 
-  def __init__(self, env, learning_rate, gamma):
-    super(SRAgent, self).__init__()
+  def __init__(self, env, latent_state_dim, learning_rate, gamma, buffer_size, batch_size,
+               epsilon, epsilon_decay, epsilon_min, device):
+    super(DSRAgent, self).__init__()
 
+    self.device = device
+    self.env = env
     self.state_dim = env.observation_space.shape
     self.action_dim = env.action_space.n
-    self.learning_rate = learning_rate
     self.gamma = gamma
+    self.epsilon = epsilon
+    self.epsilon_decay = epsilon_decay
+    self.epsilon_min = epsilon_min
+    self.batch_size = batch_size
+    self.replay_buffer = SimpleBuffer(max_size=buffer_size, device=self.device)
 
-    self.m_matrix = np.stack([np.identity(self.state_dim) for i in range(self.action_dim)])
-    self.r_vect = np.zeros((self.state_dim))
+    self.dsr_net = DSRNet(state_dim=env.observation_space["image"].shape,
+                          action_dim=env.action_space.n,
+                          latent_state_dim=latent_state_dim)
+    self.dsr_net = self.dsr_net.to(device)
+    self.optimizer = torch.optim.Adam(self.dsr_net.parameters(), lr=learning_rate)
 
-  def plan(self, observation):
-    # Calculate Q
-    pass
+  def plan(self, obs):
+    action = self._exporation()
+    if action is None:
+      obs = torch.FloatTensor(obs).float().unsqueeze(0).to(self.device)
+      self.dsr_net.eval()
+      with torch.no_grad():
+        #latent_state, w, pred_reward, reconstr_state, sr_a = self.dsr_net(obs).cup().numpy
+        m_sr_a = self.dsr_net.forward_successor_rep(obs).cup().numpy
+        w = self.dsr_net.get_w_vector()
+      self.dsr_net.train()
+      q_vals = np.stack(m_sa * w for m_sa in m_sr_a)  # TODO: check if scalar output
+      action = np.argmax(q_vals)
+    return action
 
+  def _exporation(self):
+    """Epsilon Greedy."""
+    if np.random.randn() < self.epsilon:
+      action = self.env.action_space.sample()
+    else:
+      action = None
+    self.epsilon = max(self.epsilon_min, self.epsilon * self.epsilon_decay)
+    return action
 
-  def update(self, state, reward):
-    state_idx = self._get_state_idx(state)
-    self.r_vect[state_idx] += self.learning_rate * (reward - self.r_vect)
+  def update(self, n_iterations):
+    flip_flop = True
+    for i in range(n_iterations):
+      self.optimizer.zero_grad()
+      batch = self.replay_buffer.sample(batch_size=self.batch_size)
 
-  def _get_state_idx(self, state):
-    raise NotImplementedError
+      if flip_flop:
+        loss = self._loss_latent_rep(batch)
+        self.dsr_net.deactivate_sr_path()
+        loss.backward()
+
+      else:
+        loss = self._loss_successor_rep(batch)
+        self.dsr_net.deactivate_features_path()
+        loss.backward()
+
+      self.optimizer.step()
+      self.dsr_net.activate_network()
+      flip_flop = 1 - self.flip_flop
+
+  def _loss_latent_rep(self, batch):
+    """
+    Calculate the loss for the latent representation.
+
+    This loss is decomposed in two parts:
+      - J_r = MSE(reward, calculated_reward)
+      - J_s = MSE(s, s_reconstructed)
+
+    :param batch: tuple
+      states, actions, rewards, next_states, dones
+
+    :return loss:
+      The combined loss of the reward and reconstruction heads of the neural network.
+    """
+    states, _, rewards, _, _ = batch  # TODO: Paper says reward_s_next instead of reward_s?
+    z = self.dsr_net.forward_latent_state(states)
+    reconstr_states = self.dsr_net.forward_reconstr_state(states)
+    w = self.dsr_net.get_w_vector()
+
+    rewards_target = z * w  # TODO: check if z[0] * w is scalar, total should be vector
+    loss_r = F.mse_loss(rewards, rewards_target)  # TODO (!): use received reward or calc reward?
+    loss_reconst = F.mse_loss(states, reconstr_states)
+    loss = sum(loss_r, loss_reconst)
+    return loss
+
+  def _loss_successor_rep(self, batch):
+    """
+    Calculate the loss for the latent representation.
+
+    The loss is the TD-Error: (z + gamma*m(s_next,a_next)) - m(s,a)
+
+    :return loss:
+      The loss of the neural network's head for successor features.
+    """
+    a_next = 0  # TODO: next action
+    z = 0  # TODO: laten state
+    m_sa = 0  # TODO: for argmax a from q_vals
+    m_sa_next = 0  # TODO: for argmax a_next from q_vals_next
+
+    target_m_sa_next = z + self.gamma * m_sa
+    loss = F.mse_loss(target_m_sa_next, m_sa_next)
+    return loss
+
+  def store(self, s, a, r, s_, t):
+    """
+    Store the experience tuple in the buffer
+
+    :param s: nd.array
+      State
+    :param a: int
+      Action
+    :param r: float
+      Reward
+    :param s_: nd.array
+      Next state
+    :param t: Bool
+      Terminal
+    """
+    self.replay_buffer.push(state=s, action=a, reward=r, next_state=s_, done=t)
--- src/agents/sr/dsr_agent_test.py
+++ src/agents/sr/dsr_agent_test.py
@@ -4,12 +4,12 @@ import unittest
 import numpy as np
 import torch
 
-from src.agents.sr.sr_agent import SRAgent
+from src.agents.sr.dsr_agent import DSRAgent
 
 
 class SRAgentTest(unittest.TestCase):
 
-  def mini_batch_train(self, env, agent, n_episodes, max_ep_steps):
+  def train(self, env, agent, n_episodes, max_ep_steps):
     episode_rewards = []
 
     for episode in range(n_episodes):
@@ -20,12 +20,12 @@ class SRAgentTest(unittest.TestCase):
         action = agent.plan(obs=state)
         next_state, reward, done, _ = env.step(action)
 
-        #agent.update.push(state, action, reward, next_state, done)
+        agent.store(state, action, reward, next_state, done)
 
         episode_reward += reward
 
-        if step % 4 == 0:
-          agent.update()
+        #if step % 4 == 0:
+        agent.update()
 
         if done or step == max_ep_steps - 1:
           avrg_reward = episode_reward / step
@@ -43,16 +43,31 @@ class SRAgentTest(unittest.TestCase):
     device = "cpu"
     env_id = "MiniGrid-Empty-5x5-v0"
 
-    n_episodes = 1000
-    max_ep_steps = 200
+    n_episodes = 100000
+    max_ep_steps = 1000
+
+    learning_rate = 2.5e-4
+    momentum = 0.95
+    gamma = 0.99
+    epsilon = 1
+    epsilon_decay = 0.9999
+    epsilon_min = 0.1
 
     env = gym.make(env_id)
-    agent = SRAgent(env=env)
-
-    episode_rewards = self.mini_batch_train(env=env,
-                                            agent=agent,
-                                            n_episodes=n_episodes,
-                                            max_ep_steps=max_ep_steps)
+    agent = DSRAgent(env=env,
+                     latent_state_dim=64,
+                     learning_rate=learning_rate,
+                     gamma=gamma,
+                     buffer_size=int(1e6),
+                     batch_size=128,
+                     epsilon=epsilon,
+                     epsilon_decay=epsilon_decay,
+                     epsilon_min=epsilon_min,
+                     device=device)
+    episode_rewards = self.train(env=env,
+                                 agent=agent,
+                                 n_episodes=n_episodes,
+                                 max_ep_steps=max_ep_steps)
 
     avrg_r_first_half = np.mean(np.array(
       episode_rewards[:len(episode_rewards) / 2]))
